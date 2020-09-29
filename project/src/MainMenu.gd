6164a9cb316e95ad591c1b4fbba0f2e221887533
Logansummers18 <logansummers27@att.net>
2020-09-29 10:09:19 -0400

    Added MainMenu

---
+++ project/src/MainMenu.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+
+
+
+func _on_Play_button_down():
+	get_tree().change_scene("res://src/Game.tscn")
--- project/src/win_screen.gd
+++ project/src/win_screen.gd
@@ -2,4 +2,4 @@ extends Node2D
 
 
 func _on_PlayAgain_button_down():
-	get_tree().change_scene("res://src/Game.tscn")
+	get_tree().change_scene("res://src/MainMenu.tscn")
